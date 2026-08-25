353033b587375bf1c28d77a494928254f324eb1c
Felip10xyz <felipe.rodrigues.silva06@escola.pr.gov.br>
2026-08-25 19:17:56 +0000

    salvar

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
-numeroSenha.textContent = 5;
+const botoes = document.querySelectorAll('.parametro-senha-botoes')
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    tamanhoSenha = tamanhoSenha;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+console.log(botoes);
